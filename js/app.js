57b3112852f54513f82e058562ceaa3586628b79
AdrixSC <adriana.be@outlook.com>
2018-01-18 15:27:48 -0600

    add funciton validateContact

--- js/app.js
+++ js/app.js
@@ -1,48 +1,62 @@
 
   //Traer elementos del HTML
-  
-  
+  var $nameInput = $("#name-input");
+  var $phoneInput = $("#phone-input");
+
+
+
   var loadPage = function () {
-    
-  };
-
-  var paintContactsInHTML = function(contact) {
-   
-    /* Crear elementos con DOM html al publicar contacto */
-     
-    
-    //Asignando atributos y/o eventos
-    
-
-    /* Asignando valores a los elementos*/
-    
-
-    //Agregamos lo que creamos con el DOM a un elemento existente del HTML
-    
+    $(".modal").modal();
+    $nameInput.keyup(validateContact);
+    $phoneInput.keyup(validateContact);
+    //console.log($nameInput,$phoneInput);
   };
 
   var addContact = function (e) {
     e.preventDefault();
     //Las siguientes lineas toman el valor que el usuario agrega en los inputs y los guardan en variables
-    
+
 
     //Con las variables separadas que obtuvimos creamos una estructura de datos unica, un objeto por cada contacto
-    
+
 
     /*Agregamos el contacto a nuestra data (arreglo declarado) para poderla filtrar y eliminar posteriormente*/
-    
+
     //La siguiente funcion se encarga de pintar los contactos en el html
-    
+
     /* Limpiando valores de formulario*/
-    
+
   };
-
-  
-
 
   var validateContact = function () {
     //En esta funcion tenemos que validar que el usuario ingrese datos y no valores vacios
+    //var $containerAddContact = $("#add-contact");
+
+    if ($(this).val().trim().length > 0){
+      $("#add-contact").removeAttr("disabled");
+    } else {
+      $("#add-contact").attr("disabled",true);
+    }
   };
+
+
+
+  var paintContactsInHTML = function(contact) {
+
+    /* Crear elementos con DOM html al publicar contacto */
+
+
+    //Asignando atributos y/o eventos
+
+
+    /* Asignando valores a los elementos*/
+
+
+    //Agregamos lo que creamos con el DOM a un elemento existente del HTML
+
+  };
+
+
 
 
   var filterContacts = function() {
